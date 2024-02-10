1f2a0e64df47b72df9b56e9f010b8d39115a3a66
O'Brien <abucheriobrien@gmail.com>
2024-02-10 03:49:46 +0300

    Select some Amenities to be comfortable!

---
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -0,0 +1,19 @@
+/* global $ */
+
+document.addEventListener('DOMContentLoaded', function () {
+  const amenitiesChecked = {};
+
+  $('input[type="checkbox"]').change(function () {
+    const amenityId = $(this).attr('data-id');
+    const amenityName = $(this).attr('data-name');
+
+    if (this.checked) {
+      amenitiesChecked[amenityId] = amenityName;
+    } else {
+      delete amenitiesChecked[amenityId];
+    }
+
+    const amenitiesList = Object.values(amenitiesChecked).join(', ');
+    $('div.amenities > h4').text(amenitiesList);
+  });
+});
